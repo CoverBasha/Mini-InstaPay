5ae0d2a0f6f0d0ce3788e7236d35cde6bcc1dcea
Muhab <swe.muhabfawzy@gmail.com>
2025-05-20 17:40:28 +0300

    Update Notification.cs

--- Backend/UserService/UserService/Models/Notification.cs
+++ Backend/UserService/UserService/Models/Notification.cs
@@ -3,9 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UserService.Models
 {
-    [Keyless]
     public class Notification
     {
+        [Key]
+        public int ID { get; set; }
+
         [Required]
         public int UserID { get; set; }
 
